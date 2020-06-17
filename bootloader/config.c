31a9d6ffdd7c3695b819c7ed8825186275c09e5a
Vampi <47399571+Vampitech@users.noreply.github.com>
2020-06-16 21:10:48 -0500

    Boot to OFW

--- bootloader/config.c
+++ bootloader/config.c
@@ -63,17 +63,17 @@ int create_config_entry()
 
 	LIST_INIT(ini_sections);
 
-	if (ini_parse(&ini_sections, "bootloader/hekate_ipl.ini", false))
+	if (ini_parse(&ini_sections, "NEXT/sys/ofw.ini", false))
 		mainIniFound = true;
 	else
 	{
-		u8 res = f_open(&fp, "bootloader/hekate_ipl.ini", FA_READ);
+		u8 res = f_open(&fp, "NEXT/sys/ofw.ini", FA_READ);
 		if (res == FR_NO_FILE || res == FR_NO_PATH)
 		{
-			f_mkdir("bootloader");
-			f_mkdir("bootloader/ini");
-			f_mkdir("bootloader/payloads");
-			f_mkdir("bootloader/sys");
+			f_mkdir("NEXT/sys/hkt");
+			f_mkdir("NEXT/sys/hkt/bootloader/ini");
+			f_mkdir("NEXT/sys/hkt/payloads");
+			f_mkdir("NEXT/sys/hkt/sys");
 		}
 		else
 		{
@@ -83,7 +83,7 @@ int create_config_entry()
 		}
 	}
 
-	if (f_open(&fp, "bootloader/hekate_ipl.ini", FA_WRITE | FA_CREATE_ALWAYS) != FR_OK)
+	if (f_open(&fp, "NEXT/sys/ofw.ini", FA_WRITE | FA_CREATE_ALWAYS) != FR_OK)
 		return 1;
 	// Add config entry.
 	f_puts("[config]\nautoboot=", &fp);
@@ -291,7 +291,7 @@ void config_autoboot()
 
 	if (sd_mount())
 	{
-		if (ini_parse(&ini_sections, "bootloader/hekate_ipl.ini", false))
+		if (ini_parse(&ini_sections, "NEXT/sys/ofw.ini", false))
 		{
 			// Build configuration menu.
 			ments[0].type = MENT_BACK;
@@ -367,7 +367,7 @@ void config_autoboot()
 		}
 		else
 		{
-			EPRINTF("Could not open 'bootloader/hekate_ipl.ini'.\nMake sure it exists!.");
+			EPRINTF("Could not open 'NEXT/sys/ofw.ini'.\nMake sure it exists!.");
 			goto out;
 		}
 	}
